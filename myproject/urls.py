ac68fb3afb7a53d23b8219f01b4cc685f363cdef
dabananda <imdmitra@gmail.com>
2025-06-16 00:46:24 +0600

    api redirect

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/v1/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('api.urls')),
